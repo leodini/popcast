0752b183ba6c168d225a987f294092e7352b487b
leodini <leodini@hotmail.com>
2020-05-12 03:02:34 -0300

    fixed recommendation on scroll down

--- src/components/Recommendations/Recommendations.jsx
+++ src/components/Recommendations/Recommendations.jsx
@@ -1,12 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-import Recommendation from './Recommendation'
 import { fetchRecommendations } from '../../store/fetchActions'
 import './styles.css'
 
 const Recommendations = ({ id, recommendation }) => {
+    const [scrollPosition, setScrollPosition] = useState(0)
+    const [sticky, setSticky] = useState(false)
+
     const dispatch = useDispatch()
+
+    const handleScroll = useCallback(
+        () => {
+            const position = window.pageYOffset
+            setScrollPosition(position)
+            if(scrollPosition >= 86) setSticky(true)
+            if(scrollPosition <= 80) setSticky(false)
+        }, [scrollPosition])
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true })
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [handleScroll])
 
     
     useEffect(() => {
@@ -17,11 +36,19 @@ const Recommendations = ({ id, recommendation }) => {
 
 
     return(
-        <div className="recommendations">
+        <div className={`recommendations ${sticky ? 'sticky' : ''}`}>
             <p className="recommendation-title">Recommendations</p>
             {
                 recommendationSelector && recommendationSelector.map(recommendation => (
-                    <Recommendation key={recommendation.id} recommendation={recommendation} />
+                    <div key={recommendation.id} className="recommendation">
+                        <Link to={`/podcast/${recommendation.id}`} style={{ textDecoration: 'none' }}>
+                            <img className="recommend-thumb" src={recommendation.thumbnail} alt={recommendation.title}/>
+                        </Link>
+                        <div id="title-container">
+                            <p className="recommend-title">{recommendation.title}</p>
+                            <p className="recommend-publisher">By <strong>{recommendation.publisher}</strong></p>
+                        </div>
+                    </div> 
                 ))
             }
         </div>
@@ -29,3 +56,18 @@ const Recommendations = ({ id, recommendation }) => {
 }
 
 export default Recommendations
+
+
+// const [scrollPosition, setSrollPosition] = useState(0);
+// const handleScroll = () => {
+//     const position = window.pageYOffset;
+//     setSrollPosition(position);
+// };
+
+// useEffect(() => {
+//     window.addEventListener('scroll', handleScroll, { passive: true });
+
+//     return () => {
+//         window.removeEventListener('scroll', handleScroll);
+//     };
+// }, []);
